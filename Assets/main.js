74d517c6aa3ee616c8e2e7a06a1d47c5d5f304a3
sanditor <sluque@misena.edu.co>
2026-08-20 18:51:40 -0500

    ActualizaciónV1 20Agosto2026

--- Assets/main.js
+++ Assets/main.js
@@ -2224,15 +2224,14 @@ const renderTimeSlots = () => {
       "10:00",
       "11:00",
       "12:00",
-      "13:00",
       "14:00",
       "15:00",
       "16:00",
-      "17:00",
+      "17:00"
     ];
   } else if (dayOfWeek === 6) {
     // Sábado
-    availableTimes = ["10:00", "11:00", "12:00", "13:00"];
+    availableTimes = ["10:00", "11:00", "15:00","16:00", "17:00"];
   }
 
   if (availableTimes.length === 0) {
